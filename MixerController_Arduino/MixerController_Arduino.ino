936727e2ab24747a9274ec0d253bb57af48289c2
juamunleo <2ejmunoz@gmail.com>
2021-03-07 13:31:50 +0100

    Update

--- MixerController_Arduino/MixerController_Arduino.ino
+++ MixerController_Arduino/MixerController_Arduino.ino
@@ -1,34 +1,75 @@
-#define POT_IZQ_PIN A0
-#define POT_DER_PIN A1
+#define POT_0_PIN A0
+#define POT_1_PIN A1
+#define POT_2_PIN A2
+#define POT_3_PIN A3
+#define BTN_PIN 2
 
-long POT_IZQ_VAL = 0;
-long last_POT_IZQ_VAL = 0;
-long POT_DER_VAL = 0;
-long last_POT_DER_VAL = 0;
+long BTN_LAST = 0;
+long BTN_THRESHHOLD = 500;
+
+long POT_0_VAL = 0;
+long last_POT_0_VAL = -1;
+
+long POT_1_VAL = 0;
+long last_POT_1_VAL = -1;
+
+long POT_2_VAL = 0;
+long last_POT_2_VAL = -1;
+
+long POT_3_VAL = 0;
+long last_POT_3_VAL = -1;
 
 void setup() {
-  pinMode(POT_IZQ_PIN, INPUT);
-  pinMode(POT_DER_PIN, INPUT);
+  pinMode(POT_0_PIN, INPUT);
+  pinMode(POT_1_PIN, INPUT);
+  pinMode(POT_2_PIN, INPUT);
+  pinMode(POT_3_PIN, INPUT);
+  pinMode(BTN_PIN, INPUT);
+  attachInterrupt(digitalPinToInterrupt(BTN_PIN), BTN_FUNC, RISING);
   Serial.begin(9600);
 }
 
 void loop() {
-  POT_IZQ_VAL = (long) (1023 - analogRead(POT_IZQ_PIN))*100/1023;
-  POT_DER_VAL = (long) (1023 - analogRead(POT_DER_PIN))*100/1023;
-  if(POT_IZQ_VAL != last_POT_IZQ_VAL){
-    //Serial.print("\nPotenciometro izquierdo:");
+  POT_0_VAL = (long) (1023 - analogRead(POT_0_PIN))*100/1023;
+  POT_1_VAL = (long) (1023 - analogRead(POT_1_PIN))*100/1023;
+  POT_2_VAL = (long) (1023 - analogRead(POT_2_PIN))*100/1023;
+  POT_3_VAL = (long) (1023 - analogRead(POT_3_PIN))*100/1023;
+  
+  if(POT_0_VAL != last_POT_0_VAL){
     Serial.print("0");
-    Serial.print(POT_IZQ_VAL);
+    Serial.print(POT_0_VAL);
     Serial.print("\n");
-    last_POT_IZQ_VAL = POT_IZQ_VAL;
+    last_POT_0_VAL = POT_0_VAL;
   }
 
-  if(POT_DER_VAL != last_POT_DER_VAL){
-    //Serial.print("\nPotenciometro derecho:");
+  if(POT_1_VAL != last_POT_1_VAL){
     Serial.print("1");
-    Serial.print(POT_DER_VAL);
+    Serial.print(POT_1_VAL);
     Serial.print("\n");
-    last_POT_DER_VAL = POT_DER_VAL;
+    last_POT_1_VAL = POT_1_VAL;
+  }
+
+  if(POT_2_VAL != last_POT_2_VAL){
+    Serial.print("2");
+    Serial.print(POT_2_VAL);
+    Serial.print("\n");
+    last_POT_2_VAL = POT_2_VAL;
+  }
+
+  if(POT_3_VAL != last_POT_3_VAL){
+    Serial.print("3");
+    Serial.print(POT_3_VAL);
+    Serial.print("\n");
+    last_POT_3_VAL = POT_3_VAL;
   }
   delay(100);
+}
+
+void BTN_FUNC(){
+  if(millis() - BTN_LAST > BTN_THRESHHOLD){
+    Serial.print("B");
+    Serial.print(digitalRead(BTN_PIN));
+    Serial.print("\n");
+    BTN_LAST = millis();
+  }
 }
